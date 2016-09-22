df9e6028910b66529317a77b0930f49bfa216fb2
Richard Ashkettle <richard.ashkettle@gmail.com>
2016-09-22 07:04:46 -0400

    checking in to work on it from another box

---
+++ tests/commandTests.js
@@ -0,0 +1,28 @@
+var should = require('should');
+var CommandFactory = require('../src/command');
+
+describe('Command',function(){
+	describe('should be immutable',function(){
+		let commandFactory = new CommandFactory();
+		let message = 'Original';
+		let command = commandFactory.getNewCommand(message);
+		
+		command.message.should.equal('Original');
+		
+		let timestamp = command.timestamp;
+		let correlationId = command.correlationId;
+		
+		command.message = 'New';
+
+		command.message.should.equal('Original');
+				
+		command.timestamp = Date.now();
+		command.timestamp.should.be.exactly(timestamp);
+		
+		command.correlationId = 23;
+		command.correlationId.should.be.exactly(correlationId);
+
+
+	});
+});
+
