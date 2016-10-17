a6500ecbe9e6e475a9e5f4c10ec2888dfa33ee99
Richard Ashkettle <richard.ashkettle@gmail.com>
2016-10-17 08:39:39 -0400

    finished command and event

--- src/command.js
+++ src/command.js
@@ -15,14 +15,20 @@ const _timestamp = Symbol('timestamp');
 let command = class Command {
     constructor(message) {
         let GUID = new guid();
-	    
+
         this[_correlationId] = GUID.newGUID();
         this[_message] = message;
         this[_timestamp] = Date.now();
     };
-	get correlationId() { return this[_correlationId]; }
-	get message() { return this[_message]; }
-	get timestamp() { return this[_timestamp]; }
+    get correlationId() {
+        return this[_correlationId];
+    }
+    get message() {
+        return this[_message];
+    }
+    get timestamp() {
+        return this[_timestamp];
+    }
 }
 
 module.exports = class CommandFactory {
--- src/event.js
+++ src/event.js
@@ -1,23 +1,71 @@
 let guid = require('./guid');
 
-let event = class Event {
-    constructor(eventType, message, coorelationId, version) {
-        this.message = message;
-        this.correlationId = coorelationId;
-        this.eventId = guid.newGuid();
-        this.timestamp = Date.now();
-        this.version = version;
-        this.eventType = eventType;
+const _message = Symbol('message');
+const _correlationId = Symbol('coorelationId');
+const _eventId = Symbol('eventId');
+const _timestamp = Symbol('timestamp');
+const _version = Symbol('version');
+const _eventType = Symbol('eventType');
 
+
+let event = class Event {
+    constructor(eventType, message, coorelationId, version, timestamp, eventId) {
+        this[_message] = message;
+        this[_correlationId] = coorelationId;
+        if (eventId === null) {
+            this[_eventId] = guid.newGuid();
+        } else {
+            this[_eventId] = eventId;
+        }
+        if (timestamp === null) {
+            this[_timestamp] = Date.now();
+        } else {
+            this[_timestamp] = timestamp;
+        }
+        this[_version] = version;
+        this[_eventType] = eventType;
+
+        this.serialize = () => {
+            return {
+                message: this[_message],
+                coorelationId: this[_coorelationId],
+                eventType: this[_eventType],
+                version: this[_version],
+                timestamp: this[_timestamp],
+                eventId: this[_eventId]
+            };
+        };
     };
+    get message() {
+        return this[_message];
+    }
+    get coorelationId() {
+        return this[_coorelationId];
+    }
+    get eventId() {
+        return this[_eventId];
+    }
+    get timestamp() {
+        return this[_timestamp];
+    }
+    get version() {
+        return this[_version];
+    }
+    get eventType() {
+        return this[_eventType];
+    }
 };
 
 module.export = class EventFactory {
-        constructor() {
-            this.getNewEvent = (eventType, message, coorelationId, version) => {
-                return Object.freeze(new Event(eventType, message, coorelationId, version);
+    constructor() {
+        this.getNewEvent = (eventType, message, coorelationId, version, null, null) => {
+            return Object.freeze(new Event(eventType, message, coorelationId, version));
 
-                };
-
-            };
         };
+        this.getDeserializedEvent(data) => {
+            return Object.freeze(new Event(eventType, message, coorelationId, version, timestamp, eventId));
+
+        };
+
+    };
+};
